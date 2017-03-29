abf6cc06f5703a5b1272553e3a3329ec126e3ccb
Tamas K. Papp <tkpapp@gmail.com>
2017-03-29 21:01:37 +0200

    Added Euler residual check, simplified plots.

--- src/ramsey.jl
+++ src/ramsey.jl
@@ -1,14 +1,23 @@
-using Plots
-using Parameters
-using NLsolve
-
+######################################################################
+# utility functions and setup
+######################################################################
 if ENV["USER"] == "tamas"
     cd(expanduser("~/research/exact-present/fig/"))
     plotlyjs()
 else
     warn("in your own environment, set the path and the backend")
 end
-    
+
+using Plots
+using Parameters
+using NLsolve
+
+"Save plot `plt` with given `basename`, appending appropriate extensions."
+function saveplot(plt, basename)
+    Plots.pdf(plt, basename * ".pdf")
+    PlotlyJS.savefig(plt.o, basename * ".svg")
+end
+
 """
 Very simple (normalized) Ramsey model with isoelastic production
 function and utility.
@@ -98,6 +107,52 @@ function plot_tangent!(plt, k, c01, ℓ, s=1)
     plot!(plt, k + hs, c₀ + c₁ * hs, seriestype = :line)
 end
 
+"""
+Second-order approximation for derivative through 3 points.
+
+Approximate `(x₁,y₁)`, `(x₂,y₂)`, `(0,0)` with `y = a x² + b x`, and
+return the derivative `b` at `0`.
+"""
+deriv3point(x₁, y₁, x₂, y₂) = (y₁/x₁*x₂ - y₂/x₂*x₁)/(x₂-x₁)
+
+"""
+Approximate `(x,y)` for the given 3 points with `y = a x² + b x + c`,
+and at `x[2]`.
+"""
+function deriv3point(xs, ys)
+    deriv3point(xs[1]-xs[2], ys[1]-ys[2],
+                xs[3]-xs[2], ys[3]-ys[2])
+end
+
+"""
+Return vectors of the
+
+- interior `k`s,
+- interior approximated `c′(k)`s,
+- interior Euler residuals
+
+given the `model`, `k`s, and `c`s.
+
+Uses a quadratic approximation with nearby points.
+"""
+function residual_quadapprox(model, ks, cs)
+    @unpack θ, ρ = model
+    r = zeros(length(ks)-2)
+    c′ = zeros(length(ks)-2)
+    for i in 1:(length(r))
+        ixs = i:(i+2)
+        k = ks[i+1]
+        c = cs[i+1]
+        c′[i] = deriv3point(ks[ixs], cs[ixs])
+        r[i] = c′[i]/c*(F(model, k)-c) - 1/θ*(F′(model, k)-ρ)
+    end
+    ks[2:end], c′, r
+end
+
+######################################################################
+# runtime code
+######################################################################
+
 # define a model
 model = RamseyModel(θ = 2.0, α = 0.3, A = 1.0, ρ = 0.02, δ = 0.05)
 
@@ -109,16 +164,25 @@ c01ₛ = solve_at_k(model, kₛ, guess_c01(model))
 ks = linspace(0.01*kₛ, 2*kₛ, 100)
 c01s = continuation_solver(model, ks, kₛ, c01ₛ)
 
+# approximation of c′ for nearby points
+inner_ks, inner_c′_approx, inner_r = residual_quadapprox(model, ks, c01s[1,:])
+
 # plot of policy function with steady state
 plt = plot(ks, c01s[1, :], xlab = "k", ylab = "c(k)", legend = false)
 scatter!(plt, [kₛ], [cₛ])
-Plots.pdf(plt, "ck.pdf")
-PlotlyJS.savefig(plt.o, "ck.svg")
+saveplot(plt, "ck")
 
 # plot of tangents c₁
 plt = plot(ks, c01s[1, :], xlab = "k", ylab = "c(k)", legend = false)
 for i in 10:20:length(ks)
     display(plot_tangent!(plt, ks[i], c01s[:, i], 1, 0.5))
 end
-Plots.pdf(plt, "ck-tangents.pdf")
-PlotlyJS.savefig(plt.o, "ck-tangents.svg")
+saveplot(plt, "ck-tangents")
+
+# plot of c1 and c′
+plt = plot(inner_ks, inner_c′_approx, label = "c′ from c₀", xlab="k", ylab = "c′")
+plot!(plt, inner_ks, c01s[2, 2:end], label = "c₁")
+saveplot(plt, "cderiv")
+
+plt = plot(inner_ks, inner_r, xlab="k", ylab="Euler residual", legend = false)
+saveplot(plt, "eulerresid")
