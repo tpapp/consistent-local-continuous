ec011080a1d3f037a09c8a1b6991d2badd68f8b4
Tamas K. Papp <tkpapp@gmail.com>
2017-04-02 17:49:46 +0200

    First draft.

--- src/ramsey.jl
+++ src/ramsey.jl
@@ -1,14 +1,30 @@
 ######################################################################
-# utility functions and setup
+# environment and setup
 ######################################################################
 if ENV["USER"] == "tamas"
     cd(expanduser("~/research/exact-present/fig/"))
-    plotlyjs()
 else
     warn("in your own environment, set the path and the backend")
+    "Ensure that a package is available, possibly cloning from repo."
+    function Pkg_ensure(name, repo_url)
+        if !Pkg.installed(name)
+            if Pkg.available(name)
+                Pkg.add(name)
+            else
+                Pkg.clone(repo_url)
+            end
+        end
+    end
+    Pkg.ensure("EconFunctions", "https://github.com/tpapp/EconFunctions.jl.git")
+    Pkg.ensure("ContinuousTransformations",
+               "https://github.com/tpapp/ContinuousTransformations.jl.git")
+    Pkg.ensure("ParametricFunctions", "https://github.com/tpapp/ParametricFunctions.jl.git")
 end
 
-using Plots
+using ParametricFunctions       # unregistered, clone from repo
+using ContinuousTransformations # unregistered, clone from repo
+using EconFunctions             # unregistered, clone from repo
+using Plots; plotlyjs()
 using Parameters
 using NLsolve
 
@@ -17,6 +33,10 @@ function saveplot(plt, basename)
     Plots.pdf(plt, basename * ".pdf")
     PlotlyJS.savefig(plt.o, basename * ".svg")
 end
+
+######################################################################
+# model code
+######################################################################
 
 """
 Very simple (normalized) Ramsey model with isoelastic production
@@ -38,16 +58,25 @@ F′′(model::RamseyModel, k) = (@unpack α, A = model; A*α*(α-1)*k^(α-2))
 Return a function that puts the residuals in its second argument for a
 parametrization in `model`, at capital level `k`. For use with `nlsolve`.
 """
-function residual_function(model::RamseyModel, k)
+function local_residual_function(model::RamseyModel, k)
     @unpack θ, α, A, ρ, δ = model
     Fk = F(model, k)
     F′k = F′(model, k)
     F′′k = F′′(model, k)
     function residual(c01, r)
         c₀, c₁ = c01
-        r[1] = c₁/c₀*(Fk-c₀) - 1/θ*(F′k-ρ)
+        r[1] = c₁*(Fk-c₀) - 1/θ*(F′k-ρ)*c₀
         r[2] = c₁*(F′k-c₁) - 1/θ*(F′′k*c₀ + (F′k-ρ)*c₁)
     end
+end
+
+"""
+Residual of the Euler equation.
+"""
+function euler_residual(model::RamseyModel, c, k)
+    @unpack θ, ρ, α, A, δ = model
+    ck, c′k = c(ValuePartial(k))
+    (c′k/ck)*(F(model, k)-ck) - 1/θ*(F′(model, k)-ρ)
 end
 
 "Return the steady state capital and consumption for the model."
@@ -67,11 +96,11 @@ end
 
 """
 Solve for `c₀` and `c₁` at `k`, using the initial guess. Return the
-result as a vector.
+result as a vector `[c₀,c_1]`.
 """
-function solve_at_k(model, k, initial_c01)
-    r! = residual_function(model, k)
-    sol = nlsolve(r!, initial_c01)
+function solve_at_k(model, k, initial_c01; options...)
+    r! = local_residual_function(model, k)
+    sol = nlsolve(r!, initial_c01; method = :newton, autodiff = true, options...)
     @assert NLsolve.converged(sol)
     sol.zero
 end
@@ -104,7 +133,7 @@ function plot_tangent!(plt, k, c01, ℓ, s=1)
     scatter!(plt, [k], [c₀], markersize = 2)
     h = ℓ/√(s^2+c₁^2)
     hs = [-h, h]
-    plot!(plt, k + hs, c₀ + c₁ * hs, seriestype = :line)
+    plot!(plt, k + hs, c₀ + c₁ * hs, seriestype = :line, color = "black")
 end
 
 """
@@ -146,7 +175,7 @@ function residual_quadapprox(model, ks, cs)
         c′[i] = deriv3point(ks[ixs], cs[ixs])
         r[i] = c′[i]/c*(F(model, k)-c) - 1/θ*(F′(model, k)-ρ)
     end
-    ks[2:end], c′, r
+    ks[2:(end-1)], c′, r
 end
 
 ######################################################################
@@ -158,31 +187,65 @@ model = RamseyModel(θ = 2.0, α = 0.3, A = 1.0, ρ = 0.02, δ = 0.05)
 
 # the steady state
 kₛ, cₛ = steady_state(model)
-c01ₛ = solve_at_k(model, kₛ, guess_c01(model))
+initial_c01 = guess_c01(model)
+c01ₛ = solve_at_k(model, kₛ, initial_c01)
+
+solve_at_k(model, kₛ/3, guess_c01(model); method = :newton)
 
 # solve at various k values
-ks = linspace(0.01*kₛ, 2*kₛ, 100)
+kdom = 0.2*kₛ..2*kₛ
+ks = linspace(kdom, 100)
 c01s = continuation_solver(model, ks, kₛ, c01ₛ)
 
 # approximation of c′ for nearby points
 inner_ks, inner_c′_approx, inner_r = residual_quadapprox(model, ks, c01s[1,:])
 
-# plot of policy function with steady state
-plt = plot(ks, c01s[1, :], xlab = "k", ylab = "c(k)", legend = false)
-scatter!(plt, [kₛ], [cₛ])
+# collocation
+res = CollocationResidual(model, DomainTrans(kdom, Chebyshev(10)), euler_residual)
+c_sol, o = solve_collocation(res, k->cₛ*k/kₛ; ftol=1e-10, method = :newton)
+
+######################################################################
+# plots
+######################################################################
+
+# plot of c(k), collocation vs consistent future
+plt = plot(k->c_sol(k), ks, xlab = "k", ylab = "c(k)",
+           label = "collocation", color = "black", legend = false)
+plot!(plt, ks, c01s[1, :], linestyle = :dash, label = "consistent fut.", color = "black")
+scatter!(plt, [kₛ], [cₛ], label = "steady state")
 saveplot(plt, "ck")
 
+# plot of c′(k), collocation vs consistent future
+plt = plot(k->c_sol(Partial(k)), ks, legend = false,
+           xlab = "k", ylab = "c′(k)", label = "collocation", color = "black")
+plot!(plt, ks, c01s[2, :], linestyle = :dash, label = "consistent fut.", color = "black")
+scatter!(plt, [kₛ], [c_sol(Partial(kₛ))], label = "steady state")
+saveplot(plt, "ckprime")
+
+# plot of c1 and c′
+plt = plot(inner_ks, inner_c′_approx, label = "c′ from c₀", xlab="k", ylab = "c′(k)",
+           color = "black", legend = false)
+plot!(plt, inner_ks, c01s[2, 2:end], label = "c₁", color = "black", linestyle = :dash)
+scatter!(plt, [kₛ], [c_sol(Partial(kₛ))], label = "steady state")
+saveplot(plt, "cderiv-consistentfuture")
+
+# euler residual
+plt = plot(k->euler_residual(model, c_sol, k), ks,
+           xlab="k", ylab="Euler residual", legend = false, color = "black")
+scatter!(plt, zero, points(c_sol), label = "collocation points")
+saveplot(plt, "eulerresid-collocation")
+
+# euler residual for consistent future
+plt = plot(inner_ks, inner_r, color = "black", label = "consistent fut.", xlab = "k",
+           ylab = "Euler residual", legend = false)
+scatter!(plt, zero, [kₛ], label = "steady state")
+saveplot(plt, "eulerresid-consistentfuture")
+
 # plot of tangents c₁
-plt = plot(ks, c01s[1, :], xlab = "k", ylab = "c(k)", legend = false)
+plt = plot(ks, c01s[1, :], xlab = "k", ylab = "c(k)", legend = false, color = "black",
+           linestyle = :dot)
 for i in 10:20:length(ks)
     display(plot_tangent!(plt, ks[i], c01s[:, i], 1, 0.5))
 end
-saveplot(plt, "ck-tangents")
+saveplot(plt, "ck-tangents-consistentfuture")
 
-# plot of c1 and c′
-plt = plot(inner_ks, inner_c′_approx, label = "c′ from c₀", xlab="k", ylab = "c′")
-plot!(plt, inner_ks, c01s[2, 2:end], label = "c₁")
-saveplot(plt, "cderiv")
-
-plt = plot(inner_ks, inner_r, xlab="k", ylab="Euler residual", legend = false)
-saveplot(plt, "eulerresid")
